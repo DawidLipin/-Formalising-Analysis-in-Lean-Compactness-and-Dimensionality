90a94e538178a390bb5616a6d227745e706b9ddd
Dawid Lipinski <59845110+DawidLipin@users.noreply.github.com>
2024-04-09 03:20:10 +0100

    Add files via upload

---
+++ Formalising Analysis in Lean/Formalising Analysis in Lean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `«Formalising Analysis in Lean»` library.
+-- Import modules here that should be built as part of the library.
+import «Formalising Analysis in Lean».Basic
